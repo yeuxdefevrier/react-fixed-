8da42eacb2955034ed012f770f29d16aa5e134d5
leano <s2401340@edu.bc.fi>
2026-05-20 13:22:00 +0300

    alot and new

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import InfoList from "./InfoList";
+import LinkList from "./LinkList";
+
+function App() {
+  const info = ["React", "JavaScript", "CSS"];
+
+  const links = [
+    { id: 1, name: "Google", url: "https://google.com" },
+    { id: 2, name: "React", url: "https://react.dev" },
+  ];
+
+  return (
+    <div>
+      <h1>Info List</h1>
+      <InfoList array={info} />
+
+      <h1>Link List</h1>
+      <LinkList list={links} />
+    </div>
+  );
+}
+
+export default App;
